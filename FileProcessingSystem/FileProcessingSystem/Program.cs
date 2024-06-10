0b9c262eb0dc842fa62d184c1a6797a797fac747
Polar1s <k.nadelyaev17@gmail.com>
2024-06-10 19:02:14 +0300

    Update Program.cs

--- FileProcessingSystem/FileProcessingSystem/Program.cs
+++ FileProcessingSystem/FileProcessingSystem/Program.cs
@@ -1,13 +1,13 @@
 ﻿using FileProcessingSystem;
 
-//if (args.Length < 2)
-//{
-//    Console.WriteLine("Usage: FileProcessingSystem <inputDir> <outputDir>");
-//    return;
-//}
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: FileProcessingSystem <inputDir> <outputDir>");
+    return;
+}
 
-string inputDir = "E:\\Proga\\C#\\polar1s\\FileProcessingSystem\\FileProcessingSystem\\inputFiles";
-string outputDir = "E:\\Proga\\C#\\polar1s\\FileProcessingSystem\\FileProcessingSystem\\outputFiles";
+string inputDir = args[0];
+string outputDir = args[1];
 
 var fileHandler = new FileHandler(inputDir, outputDir);
 fileHandler.ProcessAllFilesAsync();
