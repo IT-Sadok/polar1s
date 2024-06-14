b6800cc3b77e80fa92268d58d54ebd7633e52afe
Polar1s <k.nadelyaev17@gmail.com>
2024-06-14 21:20:40 +0300

    upd

--- WebScraper/WebScraper/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿/* TODO
-1) Reads a list of URLs from a file. (practice I/O from files and async)
-2) Scrapes content from each URL asynchronously. (practice concurrency and error handling)
-3) Processes the scraped content to gather text statistics (line count, word count, word frequency) (practice LINQ and working with strings).
-4) Uses a producer-consumer pattern to manage the scraping and processing concurrently. (practice concurrency)
-*/
