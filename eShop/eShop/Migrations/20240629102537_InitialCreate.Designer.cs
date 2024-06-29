da7d2dd2ffb694d54ccc419e8b2912cb103b75cf
Polar1s <k.nadelyaev17@gmail.com>
2024-06-29 13:28:43 +0300

    fixes

--- eShop/eShop/Migrations/20240629102537_InitialCreate.Designer.cs
+++ eShop/eShop/Migrations/20240629102537_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using eShop.Persistence;
 namespace eShop.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240629101743_InitialCreate")]
+    [Migration("20240629102537_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
